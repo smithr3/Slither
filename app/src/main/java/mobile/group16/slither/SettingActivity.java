3b2fe7c254113494b5c70e3492aaf5e23186ad79
SamTai95 <37604770+SamTai95@users.noreply.github.com>
2018-10-10 14:53:03 +1100

    Add files via upload

---
+++ app/src/main/java/mobile/group16/slither/SettingActivity.java
@@ -0,0 +1,18 @@
+package mobile.group16.slither;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_setting);
+        //make landscape
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+    }
+}
